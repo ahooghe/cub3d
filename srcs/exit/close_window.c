9407283feceeaa34d5b2a8bce6ab537e187c984c
brammajor <majorbram@hotmail.com>
2023-11-20 15:47:41 +0100

    yap

--- srcs/exit/close_window.c
+++ srcs/exit/close_window.c
@@ -6,24 +6,15 @@
 /*   By: brmajor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 11:26:19 by brmajor           #+#    #+#             */
-/*   Updated: 2023/11/16 13:13:05 by brmajor          ###   ########.fr       */
+/*   Updated: 2023/11/17 13:46:54 by brmajor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3d.h"
 
-int	close_window(t_data *data)
-{
-	mlx_destroy_window(data->mlx, data->win);
-	mlx_destroy_display(data->mlx);
-	free(data->mlx);
-	// more frees
-	exit(0);
-}
-
 int	close_window_key(int keycode, t_data *data)
 {
 	if (keycode == 65307)
-		close_window(data);
+		exit_cubed(data, 0);
 	return (0);
 }
